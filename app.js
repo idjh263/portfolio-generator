32fb0da411c75350d414f4780ee6f47c448ef3c5
Irene Hipolito <irenehipolito@Irenes-MacBook-Air.local>
2022-01-09 18:03:51 -0500

    input captured

---
+++ app.js
@@ -0,0 +1,41 @@
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// 
+
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//   name: 'Lernantino',
+//   age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+
+
+
+
+
+
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
+};
+
